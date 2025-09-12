d10729dea9bc51aee46a7043640a46d8d7aead60
IsaFurui <isabellefurui@gmail.com>
2025-09-12 15:25:37 -0300

    a

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,10 +4,15 @@ import bodyParser from 'body-parser';
 import wordRoutes from './routes/wordRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("API Wordle rodando 🚀");
+});
+
 app.use('/api', wordRoutes);
 
-app.listen(5000, () => console.log('Backend rodando na porta 5000'));
+app.listen(PORT, () => console.log('Backend rodando na porta 5000'));
